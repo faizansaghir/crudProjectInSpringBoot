0218d6581ed1b3c80d8a293c4bfa06568cd92574
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-11-19 20:56:08 +0530

    Added structure for CRUD Project

---
+++ src/main/java/io/github/faizansaghir/crudProjectInSpringBoot/CrudProjectInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.crudProjectInSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProjectInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProjectInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/faizansaghir/crudProjectInSpringBoot/CrudProjectInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.crudProjectInSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
