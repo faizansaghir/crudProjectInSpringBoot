75d497592ac7356d7fba5ed331fc0511e62f2eda
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-11-19 22:29:38 +0530

    Fix for adding Service annotation to EmployeeService class

--- src/main/java/io/github/faizansaghir/crudProjectInSpringBoot/service/EmployeeServiceImpl.java
+++ src/main/java/io/github/faizansaghir/crudProjectInSpringBoot/service/EmployeeServiceImpl.java
@@ -3,9 +3,11 @@ package io.github.faizansaghir.crudProjectInSpringBoot.service;
 import io.github.faizansaghir.crudProjectInSpringBoot.dao.EmployeeDao;
 import io.github.faizansaghir.crudProjectInSpringBoot.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     private final EmployeeDao employeeDao;
